b5b73d2cb38720aa14123b61141f36451836dc1c
joseggonzalezl <josegonzalez9616@hotmail.com>
2018-02-24 13:43:33 -0500

    Versión 1

---
+++ JoseGabrielGonzalez-SolarSystem/Assets/Scripts/Around.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Around : MonoBehaviour {
+
+	public Transform sunPivot;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (sunPivot.position,Vector3.up,speed);
+		
+	}
+}
